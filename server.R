70399b8069f0cd8f0e82316ac0cf939531f867be
Lucas Camarena <lucas.camarena@baobabsoluciones.es>
2021-03-14 23:53:10 +0100

    - cambios finales

--- server.R
+++ server.R
@@ -1,22 +1,91 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
 
 library(shiny)
+library(httr)
+library(visNetwork)
+library(geomnet)
+library(igraph)
+library(tidyverse)
+library(plotly)
+library(viridis)
+
+rsconnect::setAccountInfo(name='lucascamarena',
+                          token='B234121BC0C86FB9E950DFAA00A10BBF',
+                          secret='W/i9wi+kRcqg2ecGOREOb9OZWctuyLv40kvFlB4D')
+
+get_symptomas <- function(date, disease) {
+    token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJncmFyZG9sYWdhckBnbWFpbC5jb20iLCJhdWQiOiJ3ZWIiLCJuYW1lIjoiR2VyYXJkbyBMYWd1bmVzIEdhcmPDrWEiLCJ1c2VyIjp0cnVlLCJpYXQiOjE1MTM4MDY4MjB9.vGaQE6HHucr8DOM8MNnBWOCnV2dPE0r2qu_9NsYFEI-PCn5J6_iyhTUH4pwBoWCIVyvIjpNwk9vOnjQmS0-wXQ"
+    url = sprintf(
+        "http://disnet.ctb.upm.es/api/disnet/query/disnetConceptList?source=wikipedia&version=%s&diseaseName=%s&matchExactName=true&diseaseCode=&typeCode=&forceSemanticTypes=&token=%s",
+        date,
+        disease,
+        token
+    )
+    request <- httr::GET(url)
+    temp <- data.frame(date = character(), symptoms = character())
+    
+    data <- httr::content(request)
+    if (data[["responseCode"]] < 400) {
+        diseaseList <- data$diseaseList[[1]][['disnetConceptList']]
+        
+        for (sint in diseaseList) {
+            temp <-
+                rbind(temp, data.frame(date = date, symptoms = sint$name))
+        }
+    }
+    
+    return(temp)
+}
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
     output$disease <- isolate(renderText({ input$disease }))
     output$initial_date <- isolate(renderPrint({ input$initial_date }))
-    output$end_date <- isolate(renderPrint({ input$initial_date }))
+    output$end_date <- isolate(renderPrint({ input$end_date }))
     observeEvent(input$do, {
-        output$message <- renderText({ input$disease })
+        # vector with the dates
+        initial <- input$initial_date %>% as.character()
+        end <- input$end_date  %>% as.character()
+        dates <- seq(as.Date(initial), as.Date(end), by="days")
+        wd <-   dates %>% weekdays() %in% c('lunes', 'monday', 'friday', 'viernes')
+        dates <- dates[wd]
+        dis <- input$disease %>% as.character()
+        # empty dataframe
+        out <- data.frame(date = character(), symptoms = character())
+        
+        for (i in seq(1:length(dates))){
+            d = as.character(dates[i])
+            print(d)
+            out <- rbind(out, get_symptomas(d, dis))
+        }
+        
+        out <- out %>% mutate(symptom_id = symptoms %>% as.integer())
+        
+        aux <- out %>% group_by(symptoms) %>% summarise( width = n()) 
+        
+        print("aux_ok")
+        nodes1 <- aux %>% dplyr::transmute(id = symptoms,
+                                           label = symptoms) %>% rbind(data.frame(id=dis,label=dis))
+        
+        edges1 <- aux %>% transmute(from = dis, to = symptoms, width)
+        
+        output$network <- renderVisNetwork({visNetwork(nodes1, edges1) %>% visIgraphLayout() %>% 
+                visNodes(color = list(
+                    background = "#440154FF",
+                    border = "#481567FF",
+                    highlight = "#FDE725FF"
+                )) %>%
+                visEdges(color = list(color = "#440154FF", highlight = "#FDE725FF"))})
+        print("network_ok")
+        output$graph2 <-renderPlotly({
+            ggplot(data = out, aes(
+                x = date,
+                y = symptom_id,
+                color = symptoms
+            )) + geom_point() + geom_line(aes(group = symptoms)) + scale_color_viridis(discrete = T)})
+        print("graph_ok")
+        
     })
 
 })
--- ui.R
+++ ui.R
@@ -8,106 +8,29 @@
 #
 
 library(shiny)
-library(httr)
-library(jsonlite)
-
-#SYMPTOMS
-#cancer
-symptom_url <- "http://disnet.ctb.upm.es/api/disnet/query/disnetConceptList?source=wikipedia&version=2018-09-15&diseaseName=Cancer&matchExactName=true&diseaseCode=&typeCode=&forceSemanticTypes=&token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmdlbC5zYW5jaGV6LmZlcm5hbmRlei1jYXJvQGFsdW1ub3MudXBtLmVzIiwiYXVkIjoid2ViIiwibmFtZSI6IkFuZ2VsIFNhbmNoZXoiLCJ1c2VyIjp0cnVlLCJpYXQiOjE2MTUyMzE0MTN9.A1q9goQrmUDPJe0PUJRAwlUSbB4KD3TteuGLmFTCtzHHsKoggZ8m8hw5CyRp4_hJHtYvgmyCJ7fyGHWnfxqs_A"
-symptom <- GET(symptom_url)
-symptom <- fromJSON(content(symptom, type = "text"))
-cancer <- symptom[["diseaseList"]][4]
-#pancreatitis
-symptom_url <- "http://disnet.ctb.upm.es/api/disnet/query/disnetConceptList?source=wikipedia&version=2018-09-15&diseaseName=Pancreatitis&matchExactName=true&diseaseCode=&typeCode=&forceSemanticTypes=&token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmdlbC5zYW5jaGV6LmZlcm5hbmRlei1jYXJvQGFsdW1ub3MudXBtLmVzIiwiYXVkIjoid2ViIiwibmFtZSI6IkFuZ2VsIFNhbmNoZXoiLCJ1c2VyIjp0cnVlLCJpYXQiOjE2MTUyMzE0MTN9.A1q9goQrmUDPJe0PUJRAwlUSbB4KD3TteuGLmFTCtzHHsKoggZ8m8hw5CyRp4_hJHtYvgmyCJ7fyGHWnfxqs_A"
-symptom <- GET(symptom_url)
-symptom <- fromJSON(content(symptom, type = "text"))
-pancreatitis <- symptom[["diseaseList"]][4]
-#pneumonia
-symptom_url <- "http://disnet.ctb.upm.es/api/disnet/query/disnetConceptList?source=wikipedia&version=2018-09-15&diseaseName=Pneumonitis&matchExactName=true&diseaseCode=&typeCode=&forceSemanticTypes=&token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmdlbC5zYW5jaGV6LmZlcm5hbmRlei1jYXJvQGFsdW1ub3MudXBtLmVzIiwiYXVkIjoid2ViIiwibmFtZSI6IkFuZ2VsIFNhbmNoZXoiLCJ1c2VyIjp0cnVlLCJpYXQiOjE2MTUyMzE0MTN9.A1q9goQrmUDPJe0PUJRAwlUSbB4KD3TteuGLmFTCtzHHsKoggZ8m8hw5CyRp4_hJHtYvgmyCJ7fyGHWnfxqs_A"
-symptom <- GET(symptom_url)
-symptom <- fromJSON(content(symptom, type = "text"))
-pneumonia <- symptom[["diseaseList"]][4]
-#Arthritis
-symptom_url <- "http://disnet.ctb.upm.es/api/disnet/query/disnetConceptList?source=wikipedia&version=2018-09-15&diseaseName=Arthritis&matchExactName=true&diseaseCode=&typeCode=&forceSemanticTypes=&token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmdlbC5zYW5jaGV6LmZlcm5hbmRlei1jYXJvQGFsdW1ub3MudXBtLmVzIiwiYXVkIjoid2ViIiwibmFtZSI6IkFuZ2VsIFNhbmNoZXoiLCJ1c2VyIjp0cnVlLCJpYXQiOjE2MTUyMzE0MTN9.A1q9goQrmUDPJe0PUJRAwlUSbB4KD3TteuGLmFTCtzHHsKoggZ8m8hw5CyRp4_hJHtYvgmyCJ7fyGHWnfxqs_A"
-symptom <- GET(symptom_url)
-symptom <- fromJSON(content(symptom, type = "text"))
-arthritis <- symptom[["diseaseList"]][4]
-#Anterior compartment syndrome
-symptom_url <- "http://disnet.ctb.upm.es/api/disnet/query/disnetConceptList?source=wikipedia&version=2018-09-15&diseaseName=Anterior%20compartment%20syndrome&matchExactName=true&diseaseCode=&typeCode=&forceSemanticTypes=&token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmdlbC5zYW5jaGV6LmZlcm5hbmRlei1jYXJvQGFsdW1ub3MudXBtLmVzIiwiYXVkIjoid2ViIiwibmFtZSI6IkFuZ2VsIFNhbmNoZXoiLCJ1c2VyIjp0cnVlLCJpYXQiOjE2MTUyMzE0MTN9.A1q9goQrmUDPJe0PUJRAwlUSbB4KD3TteuGLmFTCtzHHsKoggZ8m8hw5CyRp4_hJHtYvgmyCJ7fyGHWnfxqs_A"
-symptom <- GET(symptom_url)
-symptom <- fromJSON(content(symptom, type = "text"))
-Anterior_compartment_syndrome <- symptom[["diseaseList"]][4]
-#Anterior cruciate ligament injury
-symptom_url <- "http://disnet.ctb.upm.es/api/disnet/query/disnetConceptList?source=wikipedia&version=2018-09-15&diseaseName=Anterior%20cruciate%20ligament%20injury&matchExactName=true&diseaseCode=&typeCode=&forceSemanticTypes=&token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmdlbC5zYW5jaGV6LmZlcm5hbmRlei1jYXJvQGFsdW1ub3MudXBtLmVzIiwiYXVkIjoid2ViIiwibmFtZSI6IkFuZ2VsIFNhbmNoZXoiLCJ1c2VyIjp0cnVlLCJpYXQiOjE2MTUyMzE0MTN9.A1q9goQrmUDPJe0PUJRAwlUSbB4KD3TteuGLmFTCtzHHsKoggZ8m8hw5CyRp4_hJHtYvgmyCJ7fyGHWnfxqs_A"
-symptom <- GET(symptom_url)
-symptom <- fromJSON(content(symptom, type = "text"))
-Anterior_cruciate_ligament_injury <- symptom[["diseaseList"]][4]
-#Epilepsy
-symptom_url <- "http://disnet.ctb.upm.es/api/disnet/query/disnetConceptList?source=wikipedia&version=2018-09-15&diseaseName=Epilepsy&matchExactName=true&diseaseCode=&typeCode=&forceSemanticTypes=&token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmdlbC5zYW5jaGV6LmZlcm5hbmRlei1jYXJvQGFsdW1ub3MudXBtLmVzIiwiYXVkIjoid2ViIiwibmFtZSI6IkFuZ2VsIFNhbmNoZXoiLCJ1c2VyIjp0cnVlLCJpYXQiOjE2MTUyMzE0MTN9.A1q9goQrmUDPJe0PUJRAwlUSbB4KD3TteuGLmFTCtzHHsKoggZ8m8hw5CyRp4_hJHtYvgmyCJ7fyGHWnfxqs_A"
-symptom <- GET(symptom_url)
-symptom <- fromJSON(content(symptom, type = "text"))
-Epilepsy <- symptom[["diseaseList"]][4]
-#Listeriosis
-symptom_url <- "http://disnet.ctb.upm.es/api/disnet/query/disnetConceptList?source=wikipedia&version=2018-09-15&diseaseName=Listeriosis&matchExactName=true&diseaseCode=&typeCode=&forceSemanticTypes=&token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmdlbC5zYW5jaGV6LmZlcm5hbmRlei1jYXJvQGFsdW1ub3MudXBtLmVzIiwiYXVkIjoid2ViIiwibmFtZSI6IkFuZ2VsIFNhbmNoZXoiLCJ1c2VyIjp0cnVlLCJpYXQiOjE2MTUyMzE0MTN9.A1q9goQrmUDPJe0PUJRAwlUSbB4KD3TteuGLmFTCtzHHsKoggZ8m8hw5CyRp4_hJHtYvgmyCJ7fyGHWnfxqs_A"
-symptom <- GET(symptom_url)
-symptom <- fromJSON(content(symptom, type = "text"))
-Listeriosis <- symptom[["diseaseList"]][4]
-
-diseases <- c(Anterior_compartment_syndrome, Anterior_cruciate_ligament_injury, arthritis, cancer, Epilepsy, Listeriosis, pancreatitis, pneumonia)
-diseases <- data.frame(diseases)
-diseases_name <- c("Anterior_compartment_syndrome", 
-                   "Anterior_cruciate_ligament_injury",
-                   "arthritis", 
-                   "cancer", 
-                   "Epilepsy", 
-                   "Listeriosis", 
-                   "pancreatitis",
-                   "pneumonia")
-
-diseases <- c(Anterior_compartment_syndrome, Anterior_cruciate_ligament_injury, arthritis, cancer, Epilepsy, Listeriosis, pancreatitis, pneumonia)
-diseases <- data.frame(diseases)
-diseases <- transpose(diseases)
-diseases_name <- c("Anterior_compartment_syndrome", 
-                   "Anterior_cruciate_ligament_injury",
-                   "arthritis", 
-                   "cancer", 
-                   "Epilepsy", 
-                   "Listeriosis", 
-                   "pancreatitis",
-                   "pneumonia")
-
-diseases_name <- data.frame(diseases_name)
-diseases_final <- data.frame(diseases_name, diseases)
-
-barplot(t(as.matrix(diseases_final[2])),beside=TRUE, main = "Number of symptoms",
-        xlab = "Diseases", ylab = "Symptoms", names.arg = c("Anterior_compartment_syndrome", 
-                                                            "Anterior_cruciate_ligament_injury",
-                                                            "arthritis", 
-                                                            "cancer", 
-                                                            "Epilepsy", 
-                                                            "Listeriosis", 
-                                                            "pancreatitis",
-                                                            "pneumonia"))
-
+library(visNetwork)
+library(plotly)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Diset visualization app"),
+    titlePanel("DISNET visualization app"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            textInput("disease", "Disease"),
+            textInput("disease", "Disease", value = 'Cancer'),
             dateInput("initial_date", "Initial date:", value = Sys.Date()-30),
-            dateInput("end_date", "Initial date:", value = Sys.Date()),
-            actionButton("do", "Search diesease")
+            dateInput("end_date", "End date:", value = Sys.Date()),
+            actionButton("do", "Search disease")
             
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            textOutput("message")
+            visNetworkOutput("network"),
+            plotlyOutput("graph2")
+            
         )
     )
 ))
